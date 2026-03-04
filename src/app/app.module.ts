b9b7cc49a8ad213db9867377050e66c017d3106d
Sai Tai Mao <109742655+SaiBooBoo@users.noreply.github.com>
2026-03-04 23:46:20 +0630

    adding routes

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { App } from "./app";
+import { ToolbarComponent } from "./toolbar/toolbar.component";
+import { Card } from "./card/card";
+import { Accordion } from "./accordion/accordion";
+import { ProgressBar } from "./progress-bar/progress-bar";
+import { StarRating } from "./star-rating/star-rating";
+import { TopOfPage } from "./top-of-page/top-of-page";
+import { RouterModule } from "@angular/router";
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule, App,
+    ToolbarComponent,
+    Card,
+    Accordion,
+    ProgressBar,
+    StarRating,
+    TopOfPage,
+    RouterModule,
+    RouterModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
