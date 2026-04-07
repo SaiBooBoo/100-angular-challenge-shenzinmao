a6a9fcbe823697f06d6af5b4e3ce836211492e73
Sai Tai Mao <109742655+SaiBooBoo@users.noreply.github.com>
2026-04-07 22:36:54 +0630

    comments

--- src/app/directive/directive-documentation/directive-documentation.ts
+++ src/app/directive/directive-documentation/directive-documentation.ts
@@ -6,8 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './directive-documentation.html',
   styleUrl: './directive-documentation.scss',
 })
-// A video about education
 export class DirectiveDocumentation {
- // something about the directiveDocumentation
+
 }
 
